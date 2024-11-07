410c32fc83b1b7b84e9201914fc095e436462c65
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-07 20:15:36 +0000

    feat(storage): add S3Service for file storage and retrieval

---
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/S3Service.java
@@ -0,0 +1,46 @@
+package team.proximity.service_provider_profile.about_your_business;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+public class S3Service implements FileStorageService {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+
+        String fileName = file.getOriginalFilename();
+
+
+        if (fileName == null || fileName.isEmpty()) {
+            throw new IllegalArgumentException("File name is missing");
+        }
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + fileName;
+    }
+}
