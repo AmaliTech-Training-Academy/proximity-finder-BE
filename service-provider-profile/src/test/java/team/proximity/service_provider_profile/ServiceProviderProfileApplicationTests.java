59226733255e1ba516ee6d7d84040e93b19fee86
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-07 15:24:53 +0000

    chore(project-setup): initial git commit with basic dependencies

---
+++ service-provider-profile/src/test/java/team/proximity/service_provider_profile/ServiceProviderProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package team.proximity.service_provider_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
