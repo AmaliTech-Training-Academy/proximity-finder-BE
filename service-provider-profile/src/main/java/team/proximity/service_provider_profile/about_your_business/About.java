4d7ae35e17f75c58484b15b96f7e19518c6caafb
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-07 16:18:11 +0000

    feature(about-service): add about entity to accept information about business

---
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/About.java
@@ -0,0 +1,31 @@
+package team.proximity.service_provider_profile.about_your_business;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class About {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String businessId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate inceptionDate;
+    @ElementCollection
+    private Set<String> socialMediaLinks;
+    private Integer numberOfEmployees;
+    private String businessCertificate;
+    private String businessSummary;
+}
