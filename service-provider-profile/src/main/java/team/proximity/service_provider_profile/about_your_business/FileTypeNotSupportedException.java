c67ceed4e7d3816eca1fc93a810ccfe9b346663c
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 09:16:56 +0000

    feat(validation): implement validation for file types

---
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/FileTypeNotSupportedException.java
@@ -0,0 +1,7 @@
+package team.proximity.service_provider_profile.about_your_business;
+
+public class FileTypeNotSupportedException extends RuntimeException {
+    public FileTypeNotSupportedException(String message) {
+        super(message);
+    }
+}
