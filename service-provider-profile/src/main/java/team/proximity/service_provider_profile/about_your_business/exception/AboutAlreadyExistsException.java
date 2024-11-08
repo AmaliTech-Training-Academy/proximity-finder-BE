6ffc0f7bf3aea031618f8cfac115ffe347137ff9
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 10:33:12 +0000

    feat(about): add AboutValidator to check for existing about entries

---
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/exception/AboutAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package team.proximity.service_provider_profile.about_your_business.exception;
+
+public class AboutAlreadyExistsException extends RuntimeException {
+    public AboutAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/validations/Validator.java
@@ -0,0 +1,5 @@
+package team.proximity.service_provider_profile.about_your_business.validations;
+
+public interface Validator<T> {
+    void validate(T input);
+}
