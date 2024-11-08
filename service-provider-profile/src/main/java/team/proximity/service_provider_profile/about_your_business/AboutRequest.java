d9951bb2c13281a4264fc30253e529fe6d2105f3
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 09:14:41 +0000

    refactor(request): add validation to request

--- service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/AboutRequest.java
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/AboutRequest.java
@@ -1,7 +1,10 @@
 package team.proximity.service_provider_profile.about_your_business;
 
+import org.springframework.validation.annotation.Validated;
+
 import java.time.LocalDate;
 import java.util.Set;
+
 
 public record AboutRequest(
         LocalDate inceptionDate,
