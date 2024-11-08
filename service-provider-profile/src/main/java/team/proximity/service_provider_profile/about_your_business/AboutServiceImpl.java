aed62bf8839d77b8aa2db98aa67dbaecc6a44e86
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 10:32:01 +0000

    feat(about): implement duplicate check and global exception handling

--- service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/AboutServiceImpl.java
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/AboutServiceImpl.java
@@ -2,6 +2,7 @@ package team.proximity.service_provider_profile.about_your_business;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import team.proximity.service_provider_profile.about_your_business.validations.AboutValidator;
 import team.proximity.service_provider_profile.about_your_business.validations.FileValidator;
 
 import java.io.IOException;
@@ -11,27 +12,31 @@ public class AboutServiceImpl implements AboutService {
     private final FileStorageService fileStorageService;
     private final AboutRepository aboutRepository;
     private final FileValidator fileValidator;
+    private final AboutValidator aboutValidator;
 
-    public AboutServiceImpl(FileStorageService fileStorageService, AboutRepository aboutRepository, FileValidator fileValidator) {
+    public AboutServiceImpl(FileStorageService fileStorageService, AboutRepository aboutRepository, FileValidator fileValidator, AboutValidator aboutValidator) {
         this.fileStorageService = fileStorageService;
         this.aboutRepository = aboutRepository;
         this.fileValidator = fileValidator;
+        this.aboutValidator = aboutValidator;
     }
 
-
     public void createOneAbout(AboutRequest aboutRequest, MultipartFile businessIdentityCardFile, MultipartFile businessCertificateFile) throws IOException {
-        fileValidator.validateFile(businessIdentityCardFile);
-        fileValidator.validateFile(businessCertificateFile);
+        aboutValidator.validate(aboutRequest);
+        fileValidator.validate(businessIdentityCardFile);
+        fileValidator.validate(businessCertificateFile);
+
         String businessIdentityCardUrl = fileStorageService.uploadFile(businessIdentityCardFile);
         String businessCertificateUrl = fileStorageService.uploadFile(businessCertificateFile);
 
-        About about = new About();
-        about.setInceptionDate(aboutRequest.inceptionDate());
-        about.setSocialMediaLinks(aboutRequest.socialMediaLinks());
-        about.setNumberOfEmployees(aboutRequest.numberOfEmployees());
-        about.setBusinessIdentityCard(businessIdentityCardUrl);
-        about.setBusinessCertificate(businessCertificateUrl);
-        about.setBusinessSummary(aboutRequest.businessSummary());
+        About about = About.builder()
+                .inceptionDate(aboutRequest.inceptionDate())
+                .socialMediaLinks(aboutRequest.socialMediaLinks())
+                .numberOfEmployees(aboutRequest.numberOfEmployees())
+                .businessIdentityCard(businessIdentityCardUrl)
+                .businessCertificate(businessCertificateUrl)
+                .businessSummary(aboutRequest.businessSummary())
+                .build();
         aboutRepository.save(about);
     }
 }
