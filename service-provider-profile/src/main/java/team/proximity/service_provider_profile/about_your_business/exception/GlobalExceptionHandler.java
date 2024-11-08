db5521def0f3a8df56fb2c8658439510c3feb0cb
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 10:36:16 +0000

    feat(exception): add logging to global exception handler

--- service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/exception/GlobalExceptionHandler.java
+++ service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package team.proximity.service_provider_profile.about_your_business.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,21 +11,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 
     @ExceptionHandler(AboutAlreadyExistsException.class)
     public ResponseEntity<String> handleAboutAlreadyExistsException(AboutAlreadyExistsException ex) {
+        LOGGER.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(FileTypeNotSupportedException.class)
     public ResponseEntity<String> handleFileTypeNotSupportedException(FileTypeNotSupportedException ex) {
+        LOGGER.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
+        LOGGER.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + ex.getMessage());
     }
 }
