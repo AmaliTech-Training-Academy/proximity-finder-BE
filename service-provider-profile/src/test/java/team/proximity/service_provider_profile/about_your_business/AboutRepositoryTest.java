a83481403a38ba49e41fd44eff93731d092e72b9
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-08 15:09:29 +0000

    feat(test): add test for about repository

---
+++ service-provider-profile/src/test/java/team/proximity/service_provider_profile/about_your_business/AboutRepositoryTest.java
@@ -0,0 +1,59 @@
+package team.proximity.service_provider_profile.about_your_business;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@Transactional
+class AboutRepositoryTest {
+
+    @Autowired
+    private AboutRepository aboutRepository;
+
+    private About about;
+
+    @BeforeEach
+    public void setUp() {
+
+        about = About.builder()
+                .inceptionDate(LocalDate.of(2020, 1, 1))
+                .socialMediaLinks(Set.of("https://facebook.com", "https://twitter.com"))
+                .numberOfEmployees(100)
+                .businessSummary("Business Summary")
+                .build();
+
+
+        aboutRepository.save(about);
+    }
+
+    @Test
+    public void testFindByInceptionDateWhenExistsShouldReturnAbout() {
+
+        LocalDate inceptionDate = about.getInceptionDate();
+        Optional<About> foundAbout = aboutRepository.findByInceptionDate(inceptionDate);
+
+        assertThat(foundAbout).isPresent();
+        assertThat(foundAbout.get().getInceptionDate()).isEqualTo(inceptionDate);
+    }
+
+    @Test
+    public void testFindByInceptionDateWhenNotExistsShouldReturnEmpty() {
+
+        LocalDate nonExistentDate = LocalDate.of(2021, 1, 1);
+        Optional<About> foundAbout = aboutRepository.findByInceptionDate(nonExistentDate);
+
+        assertThat(foundAbout).isEmpty();
+    }
+
+}
