5fadcaa4a923a7f301f0c5c887243cc33d198fca
ojAsare910 <owusufaith@gmail.com>
2024-11-10 16:28:27 +0000

    chore(JwtConstants): update access token expiration time to 30 minutes

--- authservice/src/main/java/auth/proximity/authservice/security/jwt/JwtConstants.java
+++ authservice/src/main/java/auth/proximity/authservice/security/jwt/JwtConstants.java
@@ -2,11 +2,11 @@ package auth.proximity.authservice.security.jwt;
 
 public class JwtConstants {
 
-    public static final long EXPIRE_ACCESS_TOKEN = 1 * 60 * 1000; //5 * 60 * 1000 = 5 minutes
+    public static final long EXPIRE_ACCESS_TOKEN = 30 * 60 * 1000; //5 * 60 * 1000 = 30 minutes
 
     public static final long EXPIRE_REFRESH_TOKEN = 120 * 60 * 1000;
 
-    public static final String ISSUER = "proximity";
+    public static final String ISSUER = "PROXIMITY";
 
     public static final String BEARER_PREFIX = "Bearer ";
 
