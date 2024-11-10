2b55dcb9bd5a5d96260f1745dde033bdf0d8a25c
ojAsare910 <owusufaith@gmail.com>
2024-11-10 16:25:21 +0000

    docs(LoginRequest, LoginResponse, RefreshTokenResponse): add schema annotations for login request and response DTOs, including JWT token details for improved API documentation

--- authservice/src/main/java/auth/proximity/authservice/security/dto/LoginRequest.java
+++ authservice/src/main/java/auth/proximity/authservice/security/dto/LoginRequest.java
@@ -1,11 +1,33 @@
 package auth.proximity.authservice.security.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+@Setter @Getter
+@Schema(
+        name = "Login",
+        description = "Schema to hold Login Information"
+)
 public class LoginRequest {
+
+    @NotEmpty(message = "User Email can not be a null or empty")
+    @Email
+    @Column(name = "email")
+    @Schema(
+            description = "Email of the user", example = "seeker@amalitech.org"
+    )
     private String email;
+
+    @NotEmpty(message = "Password can not be a null or empty")
+    @Size(min = 8, max = 120, message = "The length of the user password should be between 8 and 120")
+    @Column(name = "password")
+    @Schema(
+            description = "Password of the user", example = "test@1234"
+    )
     private String password;
 }
--- authservice/src/main/java/auth/proximity/authservice/security/dto/RefreshTokenResponse.java
+++ authservice/src/main/java/auth/proximity/authservice/security/dto/RefreshTokenResponse.java
@@ -5,15 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
-
-@Setter @Getter @NoArgsConstructor @AllArgsConstructor
-public class LoginResponse {
-
-    private String username;
-    private String email;
-    private List<String> roles;
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class RefreshTokenResponse {
     private String jwtAccessToken;
     private String jwtRefreshToken;
 }
