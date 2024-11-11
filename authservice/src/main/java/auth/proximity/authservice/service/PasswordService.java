e90d16d7abb9db7ffea119eabdd2f9005bc2f9a0
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-11 16:58:30 +0000

    feat(password): add password service

--- authservice/src/main/java/auth/proximity/authservice/service/PasswordService.java
+++ authservice/src/main/java/auth/proximity/authservice/service/PasswordService.java
@@ -1,29 +1,33 @@
 package auth.proximity.authservice.service;
 
+import auth.proximity.authservice.dto.ForgotPasswordRequest;
 import auth.proximity.authservice.dto.UserPasswordResetRequest;
 import auth.proximity.authservice.entity.PasswordResetToken;
 import auth.proximity.authservice.entity.User;
 import auth.proximity.authservice.repository.UserRepository;
 import auth.proximity.authservice.security.service.TokenService;
+import jakarta.mail.MessagingException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class PasswordService {
     private final TokenService tokenService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-//    private final EmailService emailService;
+    private final EmailService emailService;
 
     public PasswordService(TokenService tokenService, UserRepository userRepository, EmailService emailService, PasswordEncoder passwordEncoder) {
         this.tokenService = tokenService;
         this.userRepository = userRepository;
-//        this.emailService = emailService;
+        this.emailService = emailService;
         this.passwordEncoder = passwordEncoder;
     }
 
 
-    public String resetPassword(String token, UserPasswordResetRequest request) {
+    public void resetPassword(String token, UserPasswordResetRequest request) {
         PasswordResetToken resetToken = tokenService.validateToken(token);
 
         if (!request.password().equals(request.confirmPassword())) {
@@ -34,14 +38,13 @@ public class PasswordService {
         user.setPassword(passwordEncoder.encode(request.password()));
         userRepository.save(user);
 
-        return "Password reset successful";
     }
 
-    public void initiatePasswordReset(ResetRequest email) throws MessagingException, IOException {
-        Staff staff = userRepository.findByEmail(email.staffEmail())
-                .orElseThrow(() -> new StaffNotFoundException("Staff with email " + email + " not found"));
+    public void initiatePasswordReset(ForgotPasswordRequest forgotPasswordRequest) throws MessagingException, IOException {
+        User user = userRepository.findByEmail(forgotPasswordRequest.email())
+                .orElseThrow(() -> new IllegalArgumentException("Staff with email " + forgotPasswordRequest.email() + " not found"));
 
-        String token = tokenService.createPasswordResetToken(staff);
-        emailService.sendPasswordResetEmail(staff,token);
+        String token = tokenService.createPasswordResetToken(user);
+        emailService.sendPasswordResetEmail(user,token);
     }
 }
