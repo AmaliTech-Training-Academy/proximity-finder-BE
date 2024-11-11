11e84e1cc89afabeb7b2b87852e99c5e78b3dfdf
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-11 13:51:42 +0000

    feat(auth-service): add intial role info

---
+++ authservice/src/main/java/auth/proximity/authservice/security/InitialUserInfo.java
@@ -0,0 +1,33 @@
+package auth.proximity.authservice.security;
+
+import auth.proximity.authservice.entity.AppRole;
+import auth.proximity.authservice.entity.Role;
+import auth.proximity.authservice.entity.User;
+import auth.proximity.authservice.repository.RoleRepository;
+import auth.proximity.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class InitialUserInfo implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role seeker = roleRepository.findByRoleName(AppRole.ROLE_SEEKER)
+                .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_SEEKER)));
+        Role provider = roleRepository.findByRoleName(AppRole.ROLE_PROVIDER)
+                .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_PROVIDER)));
+        Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+    }
+}
