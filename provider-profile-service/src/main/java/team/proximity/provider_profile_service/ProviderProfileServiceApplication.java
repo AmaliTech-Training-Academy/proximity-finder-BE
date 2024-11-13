4d528495275070b16609366251a57be6095ac6df
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-13 20:40:15 +0000

    add service provider profile service

---
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/ProviderProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package team.proximity.provider_profile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderProfileServiceApplication.class, args);
+	}
+
+}
