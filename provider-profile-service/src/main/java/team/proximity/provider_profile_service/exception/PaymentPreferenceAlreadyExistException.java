aa2f43f4593857d04f9c61565f86dab340c83697
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-13 20:59:57 +0000

    add individual exceptions

---
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/exception/PaymentPreferenceAlreadyExistException.java
@@ -0,0 +1,7 @@
+package team.proximity.provider_profile_service.exception;
+
+public class PaymentPreferenceAlreadyExistException extends RuntimeException {
+    public PaymentPreferenceAlreadyExistException(String message) {
+        super(message);
+    }
+}
---
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/exception/PaymentPreferenceDoesNotExist.java
@@ -0,0 +1,7 @@
+package team.proximity.provider_profile_service.exception;
+
+public class PaymentPreferenceDoesNotExist extends RuntimeException {
+    public PaymentPreferenceDoesNotExist(String message) {
+        super(message);
+    }
+}
