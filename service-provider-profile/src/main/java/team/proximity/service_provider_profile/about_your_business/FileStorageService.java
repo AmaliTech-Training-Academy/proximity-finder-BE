23fbc49d86b5d4ddadf38bbda5e71306e93a13c5
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-13 19:38:38 +0000

    remove service provider module

--- service-provider-profile/src/main/java/team/proximity/service_provider_profile/about_your_business/FileStorageService.java
+++
@@ -1,9 +0,0 @@
-package team.proximity.service_provider_profile.about_your_business;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface FileStorageService {
-    String uploadFile(MultipartFile file) throws IOException;
-}
