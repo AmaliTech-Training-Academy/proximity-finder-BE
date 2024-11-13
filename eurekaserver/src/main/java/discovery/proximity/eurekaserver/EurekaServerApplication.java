6ffd22ca87d78562ef36e4b93afece5bd47f64b2
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-13 13:51:09 +0000

    add eureka server configurations

--- eurekaserver/src/main/java/discovery/proximity/eurekaserver/EurekaServerApplication.java
+++ eurekaserver/src/main/java/discovery/proximity/eurekaserver/EurekaServerApplication.java
@@ -2,15 +2,14 @@ package discovery.proximity.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaserverApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
