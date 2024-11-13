0d1e8f82876c6859a7b5b2a3e0aca9168fb76645
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-13 21:40:29 +0000

    configure security for default user details service

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/ProviderProfileServiceApplication.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/ProviderProfileServiceApplication.java
@@ -2,8 +2,9 @@ package team.proximity.provider_profile_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ProviderProfileServiceApplication {
 
 	public static void main(String[] args) {
