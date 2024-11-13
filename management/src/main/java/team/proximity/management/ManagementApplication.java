7b815435efe9fbc41cbce6e72da4b6d52dcd772c
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-11-13 09:47:12 +0000

    feat(managementapplication): add annotation to exclude user details service

--- management/src/main/java/team/proximity/management/ManagementApplication.java
+++ management/src/main/java/team/proximity/management/ManagementApplication.java
@@ -2,10 +2,11 @@ package team.proximity.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
 @EnableCaching
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ManagementApplication {
 
     public static void main(String[] args) {
