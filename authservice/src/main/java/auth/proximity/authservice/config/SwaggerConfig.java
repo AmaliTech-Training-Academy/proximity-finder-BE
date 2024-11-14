5c15586b1d471e132786d667bb8e3ef1523debd5
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-11-14 15:35:07 +0000

    fix(swagger-config): use environment variable in swagger config

--- authservice/src/main/java/auth/proximity/authservice/config/SwaggerConfig.java
+++ authservice/src/main/java/auth/proximity/authservice/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package auth.proximity.authservice.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.app.baseurl}")
+    private String baseUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -21,6 +26,9 @@ public class SwaggerConfig {
                                 .in(SecurityScheme.In.HEADER)
                                 .name("Authorization"))
                 )
-                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Proximity Finder User Authentication microservice REST API Documentation")
+                        .url(baseUrl + "/swagger-ui/index.html")); // Set URL dynamically
     }
 }
