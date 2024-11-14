7c92b30e056379a78137248b41bea5d339367184
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-14 15:10:52 +0000

    Initialize provider profile service application entry point

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/ProviderProfileServiceApplication.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/ProviderProfileServiceApplication.java
@@ -1,9 +1,20 @@
 package team.proximity.provider_profile_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+@OpenAPIDefinition(
+		info = @Info(
+				title = "provider profile service",
+				description = "providers wil provide their information",
+				version = "2.3"
 
+
+		)
+)
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ProviderProfileServiceApplication {
 
