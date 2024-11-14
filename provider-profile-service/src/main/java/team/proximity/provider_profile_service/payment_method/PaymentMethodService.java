c8dfe25fbbafbda10e8168aa8bc3a354d0b9660d
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-14 15:13:21 +0000

    Update PaymentMethodService interface for better abstraction

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodService.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodService.java
@@ -3,5 +3,6 @@ package team.proximity.provider_profile_service.payment_method;
 
 
 public interface PaymentMethodService {
-    void createOnePaymentMethod(PaymentMethodRequest request);
+    void createAnotherPaymentMethod(PaymentMethodRequest request);
+    void createNewPaymentMethod(PaymentMethodRequest request);
 }
