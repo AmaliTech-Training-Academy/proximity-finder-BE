4e4502f56bbb9403928688461274ed53b85c3e8a
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-14 15:12:47 +0000

    Optimize PaymentMethodRepository with better query handling

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
@@ -1,10 +1,19 @@
 package team.proximity.provider_profile_service.payment_method;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import team.proximity.provider_profile_service.payment_preference.PaymentPreference;
 
 
 import java.util.Optional;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
     Optional<PaymentMethod> findByAccountName(String name);
+
+    Optional<PaymentMethod> findByCreatedBy(String currentUsername);
+
+    @Query("SELECT p FROM PaymentMethod p WHERE p.createdBy = :createdBy AND p.paymentPreference = :paymentPreference")
+    Optional<PaymentMethod> findByCreatedByAndPaymentPreference(@Param("createdBy") String createdBy,
+                                                                @Param("paymentPreference") PaymentPreference paymentPreference);
 }
