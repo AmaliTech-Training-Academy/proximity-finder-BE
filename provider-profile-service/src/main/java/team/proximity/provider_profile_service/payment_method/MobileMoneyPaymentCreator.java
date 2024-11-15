f844a84addbf18a272f30b523055ebcc593d0930
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-15 15:35:21 +0000

    Update MobileMoneyPaymentCreator to validate required fields and throw meaningful errors

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/MobileMoneyPaymentCreator.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/MobileMoneyPaymentCreator.java
@@ -1,12 +1,16 @@
 package team.proximity.provider_profile_service.payment_method;
 
 import org.springframework.stereotype.Component;
+import team.proximity.provider_profile_service.exception.payment_method.PaymentMethodCreationException;
 
 
 @Component
 public class MobileMoneyPaymentCreator implements PaymentMethodCreator {
     @Override
     public PaymentMethod create(PaymentMethodRequest request) {
+        if (request.serviceProvider().isEmpty() || request.mobileNumber().isEmpty()) {
+            throw new PaymentMethodCreationException("All fields are required: service provider, accountName, mobile number");
+        }
         MobileMoneyServiceProvider provider = MobileMoneyServiceProvider.valueOf(request.serviceProvider().toUpperCase());
         MobileMoneyPayment mobileMoneyPayment = new MobileMoneyPayment();
         mobileMoneyPayment.setServiceProvider(provider);
