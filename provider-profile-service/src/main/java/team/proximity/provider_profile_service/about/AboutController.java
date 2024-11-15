54ffcce1fa256876be57536ad820ba7c14245872
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-15 15:31:43 +0000

    Modify AboutController to support user-specific About record operations

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutController.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutController.java
@@ -1,11 +1,12 @@
 package team.proximity.provider_profile_service.about;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import team.proximity.provider_profile_service.common.ApiSuccessResponse;
 
 import java.io.IOException;
@@ -22,12 +23,14 @@ public class AboutController {
     }
 
     @PostMapping(consumes = {"multipart/form-data"})
-    public ResponseEntity<ApiSuccessResponse> createOneAbout(@ModelAttribute AboutRequest aboutRequest) throws IOException  {
+    public ResponseEntity<ApiSuccessResponse> createOneAbout( @Valid @ModelAttribute AboutRequest aboutRequest) throws IOException {
         aboutService.createOneAbout(aboutRequest);
 
         return  ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ApiSuccessResponse("About created successfully", true));
     }
+
+
 
 }
