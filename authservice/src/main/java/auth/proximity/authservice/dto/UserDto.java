5864507731393d020edc183c5aa78510415f8769
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-11-15 10:36:19 +0000

    cleanup(userdto): remove unused import

--- authservice/src/main/java/auth/proximity/authservice/dto/UserDto.java
+++ authservice/src/main/java/auth/proximity/authservice/dto/UserDto.java
@@ -1,6 +1,5 @@
 package auth.proximity.authservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
