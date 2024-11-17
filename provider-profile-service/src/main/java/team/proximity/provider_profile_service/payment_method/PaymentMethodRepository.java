0eba8d695a9c2cee18c2b18ed46fdfb35544076c
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-17 07:23:01 +0000

    refactor: remove redundant methods in PaymentMethodRepository

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
@@ -9,9 +9,6 @@ import team.proximity.provider_profile_service.payment_preference.PaymentPrefere
 import java.util.Optional;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
-    Optional<PaymentMethod> findByAccountName(String name);
-
-    Optional<PaymentMethod> findByCreatedBy(String currentUsername);
 
     @Query("SELECT p FROM PaymentMethod p WHERE p.createdBy = :createdBy AND p.paymentPreference = :paymentPreference")
     Optional<PaymentMethod> findByCreatedByAndPaymentPreference(@Param("createdBy") String createdBy,
