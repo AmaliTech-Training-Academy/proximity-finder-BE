6c5fdfe9638caca7e65fa188b7ffb3a1131ab30f
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-17 07:23:38 +0000

    feat: enhance PaymentMethodRequest with Swagger annotations

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRequest.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRequest.java
@@ -1,12 +1,28 @@
 package team.proximity.provider_profile_service.payment_method;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PaymentMethodRequest(
+        @Schema(description = "The payment preference type. Options: Bank Account, PayPal, Mobile Money")
         String paymentPreference,
+
+        @Schema(description = "The name of the bank, required if payment preference is Bank Account", example = "Fidelity Bank")
         String bankName,
+
+        @Schema(description = "The account number, required for all payment methods", example = "123456789")
         String accountNumber,
-        String serviceProvider,
-        String mobileNumber,
+
+        @Schema(description = "The account name, required for all payment methods", example = "John Doe")
         String accountName,
-        String accountAlias
-){}
+
+        @Schema(description = "An alias for the account, optional", example = "JD123")
+        String accountAlias,
+
+        @Schema(description = "The service provider, required if payment preference is Mobile Money", example = "MTN")
+        String serviceProvider,
+
+        @Schema(description = "The mobile number, required if payment preference is Mobile Money", example = "0551234567")
+        String mobileNumber
+) {}
+
