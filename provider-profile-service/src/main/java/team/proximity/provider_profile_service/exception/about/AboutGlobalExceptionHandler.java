12a8d5383e10cae4d899084c2e6318b9ba9cf283
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-17 06:55:51 +0000

    refactor: improve error logging and method argument exceptions

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/exception/about/AboutGlobalExceptionHandler.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/exception/about/AboutGlobalExceptionHandler.java
@@ -16,93 +16,92 @@ import team.proximity.provider_profile_service.exception.payment_method.FileType
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class AboutGlobalExceptionHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(AboutGlobalExceptionHandler.class);
 
-
     @ExceptionHandler(AboutAlreadyExistsException.class)
     public ResponseEntity<ApiErrorResponse> handleAboutAlreadyExistsException(AboutAlreadyExistsException exception, HttpServletRequest request) {
+        LOGGER.error("About already exists: {}", exception.getMessage());
         ApiErrorResponse errorResponse = new ApiErrorResponse(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.CONFLICT.value(),
                 LocalDateTime.now()
         );
-        LOGGER.error(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
-
     @ExceptionHandler(FileTypeNotSupportedException.class)
     public ResponseEntity<ApiErrorResponse> handleFileTypeNotSupportedException(FileTypeNotSupportedException exception, HttpServletRequest request) {
+        LOGGER.error("File type not supported: {}", exception.getMessage());
         ApiErrorResponse errorResponse = new ApiErrorResponse(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
-        LOGGER.error(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleGenericException(Exception exception, HttpServletRequest request) {
+        LOGGER.error("An unexpected error occurred: {}", exception.getMessage());
         ApiErrorResponse errorResponse = new ApiErrorResponse(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 LocalDateTime.now()
         );
-        LOGGER.error(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(IOException.class)
     public ResponseEntity<ApiErrorResponse> handleIOException(IOException exception, HttpServletRequest request) {
+        LOGGER.error("IO Exception occurred: {}", exception.getMessage());
         ApiErrorResponse errorResponse = new ApiErrorResponse(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 LocalDateTime.now()
         );
-        LOGGER.error(exception.getMessage());
-        return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception, HttpServletRequest request) {
+        LOGGER.error("Illegal argument: {}", exception.getMessage());
         ApiErrorResponse errorResponse = new ApiErrorResponse(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
-        LOGGER.error(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(
-            MethodArgumentNotValidException ex, HttpServletRequest request) {
-
-        Map<String, String> fieldErrors = new HashMap<>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            fieldErrors.put(error.getField(), error.getDefaultMessage());
-        }
-        String combinedMessage = "Validation failed for fields: " + fieldErrors;
+    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        Map<String, List<String>> fieldErrors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ));
 
         ApiErrorResponse response = new ApiErrorResponse(
                 request.getRequestURI(),
-                combinedMessage,
+                "Validation failed: " + fieldErrors,
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+        return ResponseEntity.badRequest().body(response);
     }
-
 }
-
