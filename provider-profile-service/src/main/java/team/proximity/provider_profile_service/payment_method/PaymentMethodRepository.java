4dcc08a291c1f5e8314f56d030ebe5abe0572179
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-18 09:39:59 +0000

    feat: add method to find PaymentMethods by createdBy in PaymentMethodRepository

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/PaymentMethodRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.repository.query.Param;
 import team.proximity.provider_profile_service.payment_preference.PaymentPreference;
 
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+
+
+
+    List<PaymentMethod> findByCreatedBy(String username);
+
 
     @Query("SELECT p FROM PaymentMethod p WHERE p.createdBy = :createdBy AND p.paymentPreference = :paymentPreference")
     Optional<PaymentMethod> findByCreatedByAndPaymentPreference(@Param("createdBy") String createdBy,
