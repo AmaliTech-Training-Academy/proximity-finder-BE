7d1f0e5b11d6c42d8f31b7aad536bb88b5a60bfc
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-18 09:40:27 +0000

    fix: update MobileMoneyPaymentCreator to use phoneNumber instead of mobileNumber in request

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/MobileMoneyPaymentCreator.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/payment_method/MobileMoneyPaymentCreator.java
@@ -8,7 +8,7 @@ import team.proximity.provider_profile_service.exception.payment_method.PaymentM
 public class MobileMoneyPaymentCreator implements PaymentMethodCreator {
     @Override
     public PaymentMethod create(PaymentMethodRequest request) {
-        if (request.serviceProvider().isEmpty() || request.mobileNumber().isEmpty()) {
+        if (request.serviceProvider().isEmpty() || request.phoneNumber().isEmpty()) {
             throw new PaymentMethodCreationException("All fields are required: service provider, accountName, mobile number");
         }
         MobileMoneyServiceProvider provider = MobileMoneyServiceProvider.valueOf(request.serviceProvider().toUpperCase());
@@ -16,7 +16,7 @@ public class MobileMoneyPaymentCreator implements PaymentMethodCreator {
         mobileMoneyPayment.setServiceProvider(provider);
         mobileMoneyPayment.setAccountName(request.accountName());
         mobileMoneyPayment.setAccountAlias(request.accountAlias());
-        mobileMoneyPayment.setMobileNumber(request.mobileNumber());
+        mobileMoneyPayment.setPhoneNumber(request.phoneNumber());
         return mobileMoneyPayment;
     }
 }
