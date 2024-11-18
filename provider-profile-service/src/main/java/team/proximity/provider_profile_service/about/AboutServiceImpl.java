4b6e0f67f255b24215bfca19beeca347d9de442b
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-18 09:37:42 +0000

    feat: implement retrieval of "About Company" details for authenticated user in AboutServiceImpl

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutServiceImpl.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutServiceImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import team.proximity.provider_profile_service.common.AuthHelper;
+import team.proximity.provider_profile_service.exception.about.AboutNotFoundException;
+import team.proximity.provider_profile_service.exception.about.UnauthorizedAccessException;
 import team.proximity.provider_profile_service.upload.FileUploadService;
 import team.proximity.provider_profile_service.validations.AboutValidator;
 import team.proximity.provider_profile_service.validations.FileValidator;
@@ -18,20 +20,37 @@ public class AboutServiceImpl implements AboutService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(AboutServiceImpl.class);
 
+    private final AboutBusinessMapper aboutBusinessMapper;
     private final FileUploadService fileStorageService;
     private final AboutRepository aboutRepository;
     private final FileValidator fileValidator;
     private final AboutValidator aboutValidator;
 
-    public AboutServiceImpl(FileUploadService fileStorageService, AboutRepository aboutRepository, FileValidator fileValidator, AboutValidator aboutValidator) {
+    public AboutServiceImpl(AboutBusinessMapper aboutBusinessMapper, FileUploadService fileStorageService, AboutRepository aboutRepository, FileValidator fileValidator, AboutValidator aboutValidator) {
+        this.aboutBusinessMapper = aboutBusinessMapper;
         this.fileStorageService = fileStorageService;
         this.aboutRepository = aboutRepository;
         this.fileValidator = fileValidator;
         this.aboutValidator = aboutValidator;
     }
+    public AboutBusinessResponse getAboutForAuthenticatedUser() {
+        String authenticatedUsername = AuthHelper.getAuthenticatedUsername();
+
+        if (authenticatedUsername == null) {
+            throw new UnauthorizedAccessException("User is not authenticated.");
+        }
+
+        About about = aboutRepository.findByCreatedBy(authenticatedUsername)
+                .orElseThrow(() -> new AboutNotFoundException("No business found for the authenticated user."));
+
+        return aboutBusinessMapper.mapToResponse(about);
+    }
+
 
     @Transactional
     public void createOneAbout(AboutRequest aboutRequest) throws IOException {
+        fileValidator.validate(aboutRequest.businessIdentityCard());
+        fileValidator.validate(aboutRequest.businessCertificate());
 
         LOGGER.info("Processing About record for user: {}", AuthHelper.getAuthenticatedUsername());
 
