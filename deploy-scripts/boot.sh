ebd23b1128f9eac47393abf45101d7cf70f98df7
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-11-21 18:06:49 +0000

    chore: spell proximity

--- deploy-scripts/boot.sh
+++ deploy-scripts/boot.sh
@@ -3,6 +3,6 @@
 services=()
 
 for service in "${services[@]}"; do
-    cd /home/projects/promixity-finder/ &&
+    cd /home/projects/proximity-finder/ &&
         docker compose -f docker-compose.yml up "$service" --remove-orphans -d
 done
