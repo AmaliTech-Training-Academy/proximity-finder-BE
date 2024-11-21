a1016c7a59c4c9f0c67cd7d3dd496c5e418d16f3
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-21 17:57:17 +0000

    refactor: change id generation type to sequence

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/bank/Bank.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/bank/Bank.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class Bank {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long bankId;
     private String bankName;
 }
