cd0c6f1545a8ea6b8913f0a1ad5bcc91900f564b
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-11-22 08:58:48 +0000

    feat(logging): add request logging and spring actuator

--- management/src/main/java/team/proximity/management/config/WebConfig.java
+++ management/src/main/java/team/proximity/management/config/WebConfig.java
@@ -3,15 +3,26 @@ package team.proximity.management.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final RequestLoggingInterceptor requestLoggingInterceptor;
+
+    public WebConfig(RequestLoggingInterceptor requestLoggingInterceptor) {
+        this.requestLoggingInterceptor = requestLoggingInterceptor;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingInterceptor);
     }
 }
