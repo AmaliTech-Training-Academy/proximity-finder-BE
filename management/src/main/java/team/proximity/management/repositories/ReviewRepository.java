a49d7933dd97350ca06d313a417d039fc1f6d6c6
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-11-23 21:37:25 +0000

    feat(review): add review repository

--- management/src/main/java/team/proximity/management/repositories/ReviewRepository.java
+++ management/src/main/java/team/proximity/management/repositories/ReviewRepository.java
@@ -1,2 +1,11 @@
-package team.proximity.management.repositories;public class ReviewRepository {
+package team.proximity.management.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.proximity.management.model.Review;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByProviderService(UUID serviceProviderId);
 }
