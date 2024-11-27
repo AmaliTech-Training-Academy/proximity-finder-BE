ebbef79c5b1c8b2873aa18696f62d9bec830f895
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-11-27 16:28:39 +0000

    chore: initial project setup with basic dependencies

---
+++ request-management/src/main/java/team/proximity/request_management/request_management/QuoteAndCallManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package team.proximity.request_management.request_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteAndCallManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteAndCallManagementServiceApplication.class, args);
+	}
+
+}
