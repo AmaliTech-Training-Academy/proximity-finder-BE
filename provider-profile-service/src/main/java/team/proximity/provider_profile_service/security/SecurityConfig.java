dc26fafc4d6b59ae3224d7c320ec082cfb287fb2
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-11-28 13:25:39 +0000

    refactor: modify config for better routing

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/security/SecurityConfig.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/security/SecurityConfig.java
@@ -33,9 +33,9 @@ public class SecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/v1/banks/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"api/v1/payment-method/providers/mobile-money-providers/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"api/v1/payment-preferences/**").permitAll()
+                        .requestMatchers("/api/v1/provider-service/banks/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"api/v1/provider-service/payment-method/providers/mobile-money-providers/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"api/v1/provider-service/payment-preferences/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
