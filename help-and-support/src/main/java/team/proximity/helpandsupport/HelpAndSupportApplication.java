7df843f2e744d74eaefdf4d798217a96887bbf07
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-02 22:50:06 +0000

    chore: intial project setup with basic dependencies

---
+++ help-and-support/src/main/java/team/proximity/helpandsupport/HelpAndSupportApplication.java
@@ -0,0 +1,13 @@
+package team.proximity.helpandsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpAndSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpAndSupportApplication.class, args);
+	}
+
+}
