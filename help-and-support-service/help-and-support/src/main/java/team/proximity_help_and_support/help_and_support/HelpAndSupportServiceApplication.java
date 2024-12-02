82565446ac3e74e69dbd01fff924fdc0d5c3672a
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-02 22:36:46 +0000

    initial project setup with basic spring dependencies

---
+++ help-and-support-service/help-and-support/src/main/java/team/proximity_help_and_support/help_and_support/HelpAndSupportServiceApplication.java
@@ -0,0 +1,13 @@
+package team.proximity_help_and_support.help_and_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpAndSupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpAndSupportServiceApplication.class, args);
+	}
+
+}
---
+++ help-and-support-service/help-and-support/src/test/java/team/proximity_help_and_support/help_and_support/HelpAndSupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package team.proximity_help_and_support.help_and_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpAndSupportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
