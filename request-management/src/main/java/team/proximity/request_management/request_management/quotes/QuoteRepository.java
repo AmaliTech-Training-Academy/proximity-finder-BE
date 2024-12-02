d4dba5f22506b9b1bf975249935fb27933a9c73a
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-02 10:35:12 +0000

    feat: add method to check for user-specific duplicate quotes

--- request-management/src/main/java/team/proximity/request_management/request_management/quotes/QuoteRepository.java
+++ request-management/src/main/java/team/proximity/request_management/request_management/quotes/QuoteRepository.java
@@ -11,6 +11,8 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
     boolean existsByTitleAndAssignedProvider(String title,String assignedProvider);
 
+    boolean existsByCreatedByAndAssignedProvider(String createdBy, String assignedProvider);
+
     Optional<Quote> findByQuoteIdAndAssignedProvider(Long id, String assignedTo);
     Optional<Quote> findByQuoteIdAndCreatedBy(Long quoteId, String createdBy);
 
