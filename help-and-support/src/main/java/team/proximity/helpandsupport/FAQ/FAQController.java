ea48cbcf432300980a63063e9dc6f0f76f172e9b
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-03 16:19:01 +0000

    feat: add endpoint to fetch FAQs by type and delegate logic to service

--- help-and-support/src/main/java/team/proximity/helpandsupport/FAQ/FAQController.java
+++ help-and-support/src/main/java/team/proximity/helpandsupport/FAQ/FAQController.java
@@ -2,7 +2,6 @@ package team.proximity.helpandsupport.FAQ;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -21,6 +20,11 @@ public class FAQController {
         faqService.createFAQ(faqRequest);
         return ResponseEntity.ok(new APISuccessResponse("FAQ created successfully."));
     }
+    @GetMapping("/group")
+    public List<FAQResponse> getFAQs(@RequestParam(name = "type", required = false) String type) {
+        return faqService.getFAQsForType(type);
+    }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<APISuccessResponse> updateFAQ(@PathVariable Long id, @RequestBody FAQRequest faqRequest) {
