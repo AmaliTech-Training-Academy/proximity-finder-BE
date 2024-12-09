e5d4e0093cc09b55dd3a1730703b3bf738660e51
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-12-09 18:09:19 +0000

    chore: add rabbitmq service

--- deploy-scripts/boot.sh
+++ deploy-scripts/boot.sh
@@ -7,5 +7,17 @@ for service in "${services[@]}"; do
         docker compose -f docker-compose.yml up "$service" --remove-orphans -d
 done
 
+if docker ps --filter "name=rabbitmq-container" --format '{{.Names}}' | grep -q "rabbitmq-container"; then
+    echo "Rabbitmq container is already up and running"
+else
+    echo "Rabbitmq container is not up and running"
+    if docker compose -f docker-compose.yml up rabbitmq --remove-orphans -d; then
+            echo "RabbitMQ container started successfully"
+    else
+            echo "Failed to start RabbitMQ container"
+            exit 1
+    fi
+fi
+
 echo "Deployment complete"
 echo "All service containers are up and running 👌"
