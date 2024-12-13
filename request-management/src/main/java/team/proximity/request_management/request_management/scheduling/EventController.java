7eebd2ed3d7e6e71aeae27e0e6ffee865edd5e53
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-13 00:33:45 +0000

    chore: applied security on event endpoint

--- request-management/src/main/java/team/proximity/request_management/request_management/scheduling/EventController.java
+++ request-management/src/main/java/team/proximity/request_management/request_management/scheduling/EventController.java
@@ -3,6 +3,7 @@ package team.proximity.request_management.request_management.scheduling;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import team.proximity.request_management.request_management.quotes.ApiSuccessResponse;
 
@@ -20,6 +21,7 @@ public class EventController {
     }
 
 
+    @PreAuthorize("hasAuthority('ROLE_PROVIDER')")
     @PostMapping
     public ResponseEntity<ApiSuccessResponse> createEvent(@RequestBody EventRequest request) {
         eventService.createEvent(request);
@@ -27,27 +29,28 @@ public class EventController {
                 .body(new ApiSuccessResponse("Event created successfully"));
     }
 
-
+    @PreAuthorize("hasAuthority('ROLE_PROVIDER')")
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponse> getEvent(@PathVariable Long eventId) {
         EventResponse eventResponse = eventService.getEvent(eventId);
         return ResponseEntity.ok(eventResponse);
     }
 
-
+    @PreAuthorize("hasAuthority('ROLE_PROVIDER')")
     @GetMapping
     public ResponseEntity<List<EventResponse>> getAllEvents() {
         List<EventResponse> events = eventService.getAllEvents();
         return ResponseEntity.ok(events);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_PROVIDER')")
     @PutMapping("/{eventId}")
     public ResponseEntity<ApiSuccessResponse> updateEvent(@PathVariable Long eventId, @RequestBody EventRequest request) {
         eventService.updateEvent(eventId, request);
         return ResponseEntity.ok(new ApiSuccessResponse("Event updated successfully"));
     }
 
-
+    @PreAuthorize("hasAuthority('ROLE_PROVIDER')")
     @DeleteMapping("/{eventId}")
     public ResponseEntity<ApiSuccessResponse> deleteEvent(@PathVariable Long eventId) {
         eventService.deleteEvent(eventId);
