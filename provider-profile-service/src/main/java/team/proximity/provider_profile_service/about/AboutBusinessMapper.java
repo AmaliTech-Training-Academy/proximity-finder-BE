f861e0b0396c14dc2949e6d7b050ecd5a8f07d0f
Samuel Amo <samuel.amo@amalitechtraining.org>
2024-12-20 17:11:21 +0000

    refactor: enhance About feature with file upload integration and improved validation

--- provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutBusinessMapper.java
+++ provider-profile-service/src/main/java/team/proximity/provider_profile_service/about/AboutBusinessMapper.java
@@ -1,11 +1,34 @@
 package team.proximity.provider_profile_service.about;
 
 import org.springframework.stereotype.Component;
+import team.proximity.provider_profile_service.common.AuthHelper;
+import team.proximity.provider_profile_service.upload.FileUploadService;
 
 @Component
 public class AboutBusinessMapper {
 
-    public AboutBusinessResponse mapToResponse(About about) {
+    private final FileUploadService fileUploadService;
+
+    public AboutBusinessMapper(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    public About mapToAbout(AboutRequest aboutRequest) {
+        String businessIdentityCardPath = fileUploadService.uploadFile(aboutRequest.businessIdentityCard());
+        String businessCertificatePath = fileUploadService.uploadFile(aboutRequest.businessCertificate());
+
+        return About.builder()
+                .inceptionDate(aboutRequest.inceptionDate())
+                .socialMediaLinks(aboutRequest.socialMediaLinks())
+                .numberOfEmployees(aboutRequest.numberOfEmployees())
+                .businessIdentityCard(businessIdentityCardPath)
+                .businessCertificate(businessCertificatePath)
+                .businessSummary(aboutRequest.businessSummary())
+                .createdBy(AuthHelper.getAuthenticatedUsername())
+                .build();
+    }
+
+    public AboutBusinessResponse mapToABoutResponse(About about) {
         return new AboutBusinessResponse(
                 about.getBusinessId(),
                 about.getInceptionDate(),
